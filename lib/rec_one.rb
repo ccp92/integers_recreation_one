bfaad0c01908d6f48c6aa411a8867ce517110500
Christopher Parsons <mail@christopherparsons.me>
2018-07-26 11:35:49 +0100

    Refactoring Extracted some methods to make code easier to read.

--- lib/rec_one.rb
+++ lib/rec_one.rb
@@ -2,13 +2,17 @@
 
 class RecreationOne
   def list_squared(m, n)
-    factors_squared = (1..m).select { |n| m % n == 0 }.map! {|factor| factor ** 2}
+    factors_squared = array_of_factors(m).map! {|factor| factor ** 2}
     sum_of_squares = factors_squared.reduce(:+)
     return [m, sum_of_squares] if integer?(Math.sqrt(sum_of_squares))
     nil
   end
 
   private
+
+  def array_of_factors(m)
+    (1..m).select { |n| m % n == 0 }
+  end
 
   def integer?(num)
     num % 1 == 0
