604f83e1ce1e5880bd2b0209e2648e2a47f6fcc4
Christopher Parsons <mail@christopherparsons.me>
2018-07-26 11:27:46 +0100

    Method for finding numbers who should return an array. Method has been created to find if a single number's factors squared and summed is a square itself.

--- lib/rec_one.rb
+++ lib/rec_one.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class RecreationOne
+  def list_squared(m, n)
+    factors_squared = (1..m).select { |n| m % n == 0 }.map! {|factor| factor ** 2}
+    sum_of_squares = factors_squared.reduce(:+)
+    return [m, sum_of_squares] if integer?(Math.sqrt(sum_of_squares))
+    nil
+  end
+
+  private
+
+  def integer?(num)
+    num % 1 == 0
+  end
 end
--- spec/rec_one_spec.rb
+++ spec/rec_one_spec.rb
@@ -3,4 +3,18 @@
 require 'rec_one'
 
 describe RecreationOne do
+  let(:recreation) { RecreationOne.new }
+  context 'given two arguments that are the same' do
+    it 'can pass 1 and return the result of [1, 1]' do
+      expect(recreation.list_squared(1, 1)).to eq([1, 1])
+    end
+
+    it 'can pass 2 and return nill' do
+      expect(recreation.list_squared(2, 2)).to eq(nil)
+    end
+
+    it 'can pass 42 and return [42, 2500]' do
+      expect(recreation.list_squared(42, 42)).to eq([42, 2500])
+    end
+  end
 end
