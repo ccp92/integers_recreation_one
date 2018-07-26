7e4265664ff7b6ac2e7c65bafef5a0dac79f7aa7
Christopher Parsons <mail@christopherparsons.me>
2018-07-26 12:11:36 +0100

    Refactor Did some refactoring to the final method for this kata

--- lib/rec_one.rb
+++ lib/rec_one.rb
@@ -3,22 +3,24 @@
 class RecreationOne
   def list_squared(m, n)
     results = []
-    (m..n).each do |number|
-      factors_squared = array_of_factors(number).map! {|factor| factor ** 2}
-      sum_of_squares = factors_squared.reduce(:+)
-      results.push([number, sum_of_squares]) if integer?(Math.sqrt(sum_of_squares))
-    end
+    (m..n).each { |number| build_results(results, number) }
     return results unless results.empty?
     nil
   end
 
   private
 
+  def build_results(results, number)
+    factors_squared = array_of_factors(number).map! { |factor| factor**2 }
+    sum_of_squares = factors_squared.reduce(:+)
+    results.push([number, sum_of_squares]) if integer?(Math.sqrt(sum_of_squares))
+  end
+
   def array_of_factors(m)
-    (1..m).select { |n| m % n == 0 }
+    (1..m).select { |n| (m % n).zero? }
   end
 
   def integer?(num)
-    num % 1 == 0
+    (num % 1).zero?
   end
 end
--- spec/rec_one_spec.rb
+++ spec/rec_one_spec.rb
@@ -4,35 +4,48 @@ require 'rec_one'
 
 describe RecreationOne do
   let(:recreation) { RecreationOne.new }
+
+  result1 = [1, 1]
+  result42 = [42, 2500]
+  result246 = [246, 84_100]
+
   context 'given two arguments that are the same' do
-    it 'can pass 1 and return the result of [1, 1]' do
-      expect(recreation.list_squared(1, 1)).to eq([[1, 1]])
+    it 'can pass 1 and return [1, 1]' do
+      test(1, 1, [result1])
     end
 
     it 'can pass 2 and return nill' do
-      expect(recreation.list_squared(2, 2)).to eq(nil)
+      test(2, 2, nil)
     end
 
     it 'can pass 42 and return [42, 2500]' do
-      expect(recreation.list_squared(42, 42)).to eq([[42, 2500]])
+      test(42, 42, [result42])
     end
   end
 
-  context 'given two arguments' do
-    it 'can pass 1 and 41 and return the results of [1, 1]' do
-      expect(recreation.list_squared(1, 41)).to eq([[1, 1]])
-    end
+  context 'given two different arguments' do
+    context 'the arguments are valid (1 <= m <= n)' do
+      it 'can pass 1 and 41 and return [1, 1]' do
+        test(1, 41, [result1])
+      end
 
-    it 'can pass 41 and 42 and return the results of [42, 2500]' do
-      expect(recreation.list_squared(41, 42)).to eq([[42, 2500]])
-    end
+      it 'can pass 41 and 42 and return [42, 2500]' do
+        test(41, 42, [result42])
+      end
 
-    it 'can pass 1 and 42 and return the results of [[1, 1], [42, 2500]]' do
-      expect(recreation.list_squared(1, 42)).to eq([[1, 1], [42, 2500]])
-    end
+      it 'can pass 1 and 42 and return [[1, 1], [42, 2500]]' do
+        test(1, 42, [result1, result42])
+      end
 
-    it 'can pass 1 and 250 and return the results of [[1, 1], [42, 2500], [246, 84100]]' do
-      expect(recreation.list_squared(1, 250)).to eq([[1, 1], [42, 2500], [246, 84100]])
+      it 'can pass 1 and 250 and return [[1, 1], [42, 2500], [246, 84100]]' do
+        test(1, 250, [result1, result42, result246])
+      end
     end
+  end
+
+  private
+
+  def test(m, n, output)
+    expect(recreation.list_squared(m, n)).to eq(output)
   end
 end
